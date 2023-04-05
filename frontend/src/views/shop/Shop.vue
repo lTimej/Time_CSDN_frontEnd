3ce61b34f9741a3dc89c56f4db388319f7d0901e
lTimej <lTimej@126.com>
2023-04-05 09:53:27 +0800

    spu列表

--- frontend/src/views/shop/Shop.vue
+++ frontend/src/views/shop/Shop.vue
@@ -15,18 +15,26 @@
                 <i class="el-icon-circle-plus"></i>
             </div>
         </nav-bar>
-        <scroll
-            class="content1"
-            ref="scroll"
-        >
-            <category></category>
-        </scroll>
-        <scroll
-            class="content2"
-            ref="scroll"
-        >
-            <shop-item></shop-item>
-        </scroll>
+        <nav-bar class="shop-item-nav-copy" v-show="nav_show">
+            <div slot="left" :class="{'sort-show':sort=='zonghe'}" @click="">综合</div>
+            <div slot="center" :class="{'sort-show':sort=='sales'}">销量</div>
+            <div slot="right" :class="{'sort-show':sort=='newthing'}">新品</div>
+        </nav-bar>
+        <div class="shop-content">
+            <scroll
+                class="content1"
+                ref="scroll"
+            >
+                <category></category>
+            </scroll>
+            <scroll
+                class="content2"
+                ref="scroll"
+                @scroll="myscroll"
+            >
+                <shop-item :nav_show="nav_show"></shop-item>
+            </scroll>
+        </div>
     </div>
 </template>
 
@@ -45,8 +53,21 @@
         },
         data(){
             return{
-                search:""
+                search:"",
+                nav_show:false,
+                sort:"zonghe"
             }
+        },
+        methods:{
+            myscroll(pos){
+                console.log(pos)
+                if(pos.y < -310){
+                    this.nav_show = true
+                }else{
+                    this.nav_show = false
+                }
+                console.log("nav_show:",this.nav_show)
+            },
         }
     }
 </script>
@@ -91,6 +112,7 @@
         bottom: 49px;
         width: 20%;
         text-align: center;
+        border-top: solid 1px gray;
         background-color: rgb(245, 242, 242);
     }
     .content2{
@@ -98,13 +120,32 @@
         position: absolute;
         overflow: hidden;
         top: 49px;
-        left: 100px;
+        /* left: 100px; */
         right: 0;
         bottom: 49px;
-        width: 70%;
+        width: 80%;
         text-align: center;
     }
     .shop-cate{
         padding: 10px 0;
+    }
+    .shop-item-nav-copy{
+        position: fixed;
+        right: 0;
+        left: 20%;
+        top: 49px;
+        bottom: 0;
+        z-index: 9;
+        text-align: center;
+        border-top: solid 1px gray;
+        border-bottom: solid 1px gray;
+        background-color: white;
+    }
+    .shop-content{
+        border-top: solid 1px gray;
+        margin-top: 49px;
+    }
+    .sort-show{
+        color: red;
     }
 </style>
--- frontend/src/views/shop/ShopItem.vue
+++ frontend/src/views/shop/ShopItem.vue
@@ -30,44 +30,121 @@
                 <div>9.9秒杀</div>
             </div>
         </div>
-        <nav-bar class="shop-item-nav">
+        <nav-bar class="shop-item-nav" v-show="!nav_show">
             <div slot="left">综合</div>
             <div slot="center">销量</div>
             <div slot="right">新品</div>
         </nav-bar>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
-        <div>哈哈哈哈哈哈 </div>
+        <div class="shop-spu">
+            <div class="shop-spu-item">
+                <div class="shop-img">
+                    <img src="~assets/img/my.jpg">
+                </div>
+                <div class="shop-spu-info">
+                    <div class="shop-spu-title">
+                        <span>衣架落地立式衣柜卧室专用</span>
+                    </div>
+                    <div class="shop-spu-spec">
+                        <span class="spu-price">￥73.5</span>
+                        <div class="spu-collection">
+                            <span class="spu-collection-num">0</span>
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="shop-spu-item">
+                <div class="shop-img">
+                    <img src="~assets/img/my.jpg">
+                </div>
+                <div class="shop-spu-info">
+                    <div class="shop-spu-title">
+                        <span>衣架落地立式衣柜卧室专用</span>
+                    </div>
+                    <div class="shop-spu-spec">
+                        <span class="spu-price">￥73.5</span>
+                        <div class="spu-collection">
+                            <span class="spu-collection-num">0</span>
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="shop-spu-item">
+                <div class="shop-img">
+                    <img src="~assets/img/my.jpg">
+                </div>
+                <div class="shop-spu-info">
+                    <div class="shop-spu-title">
+                        <span>衣架落地立式衣柜卧室专用</span>
+                    </div>
+                    <div class="shop-spu-spec">
+                        <span class="spu-price">￥73.5</span>
+                        <div class="spu-collection">
+                            <span class="spu-collection-num">0</span>
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="shop-spu-item">
+                <div class="shop-img">
+                    <img src="~assets/img/my.jpg">
+                </div>
+                <div class="shop-spu-info">
+                    <div class="shop-spu-title">
+                        <span>衣架落地立式衣柜卧室专用</span>
+                    </div>
+                    <div class="shop-spu-spec">
+                        <span class="spu-price">￥73.5</span>
+                        <div class="spu-collection">
+                            <span class="spu-collection-num">0</span>
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="shop-spu-item">
+                <div class="shop-img">
+                    <img src="~assets/img/my.jpg">
+                </div>
+                <div class="shop-spu-info">
+                    <div class="shop-spu-title">
+                        <span>衣架落地立式衣柜卧室专用</span>
+                    </div>
+                    <div class="shop-spu-spec">
+                        <span class="spu-price">￥73.5</span>
+                        <div class="spu-collection">
+                            <span class="spu-collection-num">0</span>
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="shop-spu-item">
+                <div class="shop-img">
+                    <img src="~assets/img/my.jpg">
+                </div>
+                <div class="shop-spu-info">
+                    <div class="shop-spu-title">
+                        <span>衣架落地立式衣柜卧室专用</span>
+                    </div>
+                    <div class="shop-spu-spec">
+                        <span class="spu-price">￥73.5</span>
+                        <div class="spu-collection">
+                            <span class="spu-collection-num">0</span>
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -77,6 +154,12 @@
         name: "ShopItem",
         components:{
             NavBar,
+        },
+        props:{
+            nav_show:{
+                type:Boolean,
+                default:false
+            }
         },
         data(){
             return{
@@ -93,18 +176,59 @@
     .shop-item .shop-sub{
         display: flex;
         flex-wrap: wrap;
+        display: -webkit-flex;
     }
     .shop-item .shop-sub-item{
         /* line-height: 80px; */
-       padding-left: 5px;
+       padding-left: 8px;
     }
     .shop-item .shop-sub-item img{
-        width: 80px;
-        height: 80px;
+        width: 90px;
+        height: 90px;
         border-radius: 50%;
     }
     .shop-item-nav{
         border-top: solid 1px gray;
         border-bottom: solid 1px gray;
+    }
+    .shop-spu{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .shop-spu .shop-spu-item{
+        padding-left: 6px;
+        padding-top: 6px;
+    }
+    .shop-spu .shop-spu-item img{
+        width: 140px;
+        height: 180px;
+    }
+    .shop-spu-info{
+        width: 140px;
+        text-align: center;
+    }
+    .shop-spu-title{
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp:1;
+        -webkit-box-orient: vertical;
+        width: 120px;
+        overflow: hidden;
+        font-size: 13px;
+    }
+    .shop-spu-spec{
+        
+    }
+    .shop-spu-spec .spu-price{
+        float: left;
+        color: red;
+        font-size: 18px;
+    }
+    .shop-spu-spec .spu-collection{
+        float: right;
+        padding-right: 3px;
+    }
+    .shop-spu-spec .spu-collection .spu-collection-num{
+        padding: 3px;
     }
 </style>
